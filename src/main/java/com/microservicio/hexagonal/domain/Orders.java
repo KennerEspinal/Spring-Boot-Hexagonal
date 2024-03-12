56b89e67a4b1128cebf582129c5729027f684cb6
KennerEspinal <kennerespinalg@gmail.com>
2024-03-11 22:45:59 -0500

    Domain

---
+++ src/main/java/com/microservicio/hexagonal/domain/Orders.java
@@ -0,0 +1,15 @@
+package com.microservicio.hexagonal.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+@Builder
+@Getter
+@Setter
+public class Orders {
+    private String id;
+    private String customerId;
+    private BigDecimal total;
+}
