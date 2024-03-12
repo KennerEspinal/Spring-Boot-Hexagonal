3ca79608577a4a603ff794dd1fc4419b6bda0e4b
KennerEspinal <kennerespinalg@gmail.com>
2024-03-11 23:00:02 -0500

    OutPutPort

---
+++ src/main/java/com/microservicio/hexagonal/infraestructure/outputport/EntityRepository.java
@@ -0,0 +1,11 @@
+package com.microservicio.hexagonal.infraestructure.outputport;
+
+import java.util.List;
+
+public interface EntityRepository {
+    public <T> T save(T reg);
+
+    public <T> T getById(String id, Class<T> clazz);
+
+    public <T> List<T> getAll( Class<T> clazz);
+}
