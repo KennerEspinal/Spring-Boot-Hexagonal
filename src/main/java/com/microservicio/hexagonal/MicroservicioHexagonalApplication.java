9ddadf40493ea3ceedc1cdc9ccec913da7b02f6c
KennerEspinal <kennerespinalg@gmail.com>
2024-03-11 22:04:23 -0500

    Microservicio Hexagonal

---
+++ src/main/java/com/microservicio/hexagonal/MicroservicioHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservicio/hexagonal/MicroservicioHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
